cea4dcae75995d80dd30281de4e35fe8e7a5263f
xcharset <xcharset@163.com>
2020-03-20 02:04:35 +0800

    手动实现深拷贝

--- common.js
+++ common.js
@@ -247,3 +247,31 @@
             }
         }
     }
+
+    /**
+     * @desc 深拷贝
+     * @param target 拷贝目标
+     */
+    function deepClone(target) {
+        let result;
+        if(typeof target === 'object') {
+            if(Array.isArray(target)) {
+                result = [];
+                for(let i in target) {
+                    result[i] = deepClone(target[i]);
+                }
+            } else if(target === null) {
+                result = null
+            } else if(target.constructor === RegExp) {
+                result = target
+            } else {
+                result = {}
+                for(let i in target) {
+                    result[i] = deepClone(target[i])
+                }
+            }
+        } else {
+            result = target;
+        }
+        return result;
+    }
